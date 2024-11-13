3fdda0fa4b0cf2c3be4a51a3c69709f60129ca1e
John Paul San Diego <johnpaulsandiego@Johns-MacBook-Pro.local>
2024-11-13 13:21:09 +0000

    Combined both

--- tutorials/management/commands/seed.py
+++ tutorials/management/commands/seed.py
@@ -6,7 +6,7 @@ from random import randint, random
 
 user_fixtures = [
     {'username': '@johndoe', 'email': 'john.doe@example.org', 'first_name': 'John', 'last_name': 'Doe', 'user_type': 'Admin'},
-    {'username': '@janedoe', 'email': 'jane.doe@example.org', 'first_name': 'Jane', 'last_name': 'Doe', 'user_type': 'Admin'},
+    {'username': '@janedoe', 'email': 'jane.doe@example.org', 'first_name': 'Jane', 'last_name': 'Doe', 'user_type': 'Tutor'},
     {'username': '@charlie', 'email': 'charlie.johnson@example.org', 'first_name': 'Charlie', 'last_name': 'Johnson', 'user_type': 'Student'},
 ]
 
