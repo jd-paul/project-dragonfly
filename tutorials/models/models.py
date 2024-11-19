d81a63c696bd4aea8fc396bc92dff98a8ff6a2f4
anahitasav <anahitasavpour@gmail.com>
2024-11-19 23:19:11 +0000

    'add models'

--- tutorials/models/models.py
+++ tutorials/models/models.py
@@ -50,3 +50,98 @@ class User(AbstractUser):
     def mini_gravatar(self):
         """Return a URL to a miniature version of the user's gravatar."""
         return self.gravatar(size=60)
+
+
+class Admin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Tutor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class SkillLevel(models.TextChoices):
+    
+    BEGINNER = 'Beginner', 'Beginner'
+    INTERMEDIATE = 'Intermediate', 'Intermediate'
+    ADVANCED = 'Advanced', 'Advanced'
+
+
+class Skill(models.Model):
+    language = models.CharField(max_length=150, blank=False)
+    level = models.CharField(max_length=15, choices=SkillLevel.choices)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['language', 'level'], name='unique_language_level')
+        ]
+
+
+class TutorSkill(models.Model):
+    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE, related_name='skills')
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name='tutors')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['tutor', 'skill'], name='unique_tutor_skill')
+        ]
+
+
+class Term(models.TextChoices):
+    SEPTEMBER_CHRISTMAS = 'September-Christmas', 'September-Christmas'
+    JANUARY_EASTER = 'January-Easter', 'January-Easter'
+    MAY_JULY = 'May-July', 'May-July'
+
+
+class Frequency(models.TextChoices):
+    WEEKLY = 'weekly'
+    BI_WEEKLY = 'bi-weekly'
+    TWO_DAYS = '2 per week'
+    THREE_DAYS = '3 per week'
+    FOUR_DAYS = '4 per week'
+    FIVE_DAYS = '5 per week'
+    SIX_DAYS = '6 per week'
+    SEVEN_DAYS = '7 per week'
+
+
+class Day(models.Model):
+    day_name = models.CharField(max_length=20)
+
+
+class StudentRequest(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='requests')
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name='requests')
+    duration = models.TimeField()
+    term = models.CharField(max_length=60, choices=Term.choices)
+    frequency = models.CharField(max_length=20, choices=Frequency.choices)
+    
+
+class Enrollment(models.Model):
+    approved_request = models.ForeignKey(StudentRequest, on_delete=models.CASCADE, related_name='enrollments')
+    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE, related_name='enrollments')
+    start_time = models.DateTimeField()
+    status = models.CharField(max_length=50, choices=[('ongoing', 'Ongoing'), ('completed', 'Completed')])
+
+
+class EnrollmentDays(models.Model):
+    day_name =  models.ForeignKey(Day, on_delete=models.CASCADE, related_name='enrollments')
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE, related_name='days')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['day_name', 'enrollment'], name='unique_day_enrollment')
+        ]
+
+class Invoice(models.Model):
+    enrollment = models.OneToOneField(Enrollment, on_delete=models.CASCADE, related_name='invoice')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    issued_date = models.DateTimeField()
+    payment_status = models.CharField(max_length=50, choices=[('paid', 'Paid'), ('unpaid', 'Unpaid')])
+    due_date = models.DateTimeField()
+
+# add created and modified date to every table
+
