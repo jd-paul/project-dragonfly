21ce3dd71f4a046241bbb64af8681db1b266477e
anahitasav <anahitasavpour@gmail.com>
2024-12-11 18:40:50 +0000

    add is_tutor test tweak test_admin_sees_empty

--- tutorials/tests/views/test_helpers.py
+++ tutorials/tests/views/test_helpers.py
@@ -1,9 +1,7 @@
 from django.test import TestCase
 from django.core.exceptions import PermissionDenied
 from tutorials.models import User
-from tutorials.views import is_admin, is_student
-
-
+from tutorials.views import is_admin, is_student, is_tutor
 
 
 class HelperFunctionsTestCase(TestCase):
@@ -28,9 +26,14 @@ class HelperFunctionsTestCase(TestCase):
     def test_is_student_with_student_user(self):
         self.assertTrue(is_student(self.student))
 
+    def test_is_student_with_non_student_raises(self):
+        with self.assertRaises(PermissionDenied):
+            is_student(self.admin)
+        with self.assertRaises(PermissionDenied):
+            is_student(self.tutor)
 
-   def test_is_student_with_non_student_raises(self):
-       with self.assertRaises(PermissionDenied):
-           is_student(self.admin)
-       with self.assertRaises(PermissionDenied):
-           is_student(self.tutor)
+    def test_is_tutor_with_non_tutor_raises(self):
+        with self.assertRaises(PermissionDenied):
+            is_tutor(self.admin)
+        with self.assertRaises(PermissionDenied):
+            is_tutor(self.student)
